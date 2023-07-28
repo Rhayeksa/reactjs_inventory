3e00e4ae4dc7fcdfc141530b4b865661438eee4e
rhayeksa <rhayeksads@gmail.com>
2023-07-28 23:40:42 +0700

    finish develop pages

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,24 +1,28 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
+import { CardListComponent } from "../components/cardListComponent";
 import { NavbarComponent } from "../components/navbarComponent";
-import { CardComponent } from "../components/cardComponent";
 
 const Home = () => {
-    return (
-        <div>
-            <NavbarComponent />
-            <Container maxWidth="xl">
-                <h1>Home Page</h1>
-                <CardComponent />
-
-                <h1>TODO List</h1>
-                <ul>
-                    <li>Recent Item</li>
-                    <li>Item dengan quantity terbanyak</li>
-                    <li>Item dengan quantity terrendah</li>
-                </ul>
-            </Container>
-        </div>
-    )
-}
+  return (
+    <div>
+      <NavbarComponent />
+      <Container maxWidth="xl">
+        <h1>New Item</h1>
+        <Box display={"flex"} flexWrap={"wrap"}>
+          <CardListComponent name="Item 1" quantity={30} />
+          <CardListComponent name="Item 1" quantity={30} />
+          <CardListComponent name="Item 1" quantity={30} />
+          <CardListComponent name="Item 1" quantity={30} />
+        </Box>
+        <h1>TODO List</h1>
+        <ul>
+          <li>show Item desc by id (item terbaru)</li>
+          <li>Item dengan quantity terbanyak</li>
+          <li>Item dengan quantity terrendah</li>
+        </ul>
+      </Container>
+    </div>
+  );
+};
 
 export { Home };
--- src/pages/item/add.tsx
+++ src/pages/item/add.tsx
@@ -1,53 +1,55 @@
-import { Box, Button, Container, TextField } from "@mui/material";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import SaveIcon from "@mui/icons-material/Save";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import { NavbarComponent } from "../../components/navbarComponent";
 
 const Add = () => {
-    return (
-        <>
-            <NavbarComponent />
-            <Container maxWidth="xl">
-                <h1>Form Add Item</h1>
-                <TextField
-                    required
-                    id="item-name"
-                    label="Name"
-                    focused
-                    sx={{ mb: 3 }}
-                    fullWidth
-                />
-                <TextField
-                    id="outlined-number"
-                    label="Quantity"
-                    type="number"
-                    required
-                    fullWidth
-                    focused
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    sx={{ mb: 3 }}
-                />
-                <TextField
-                    id="item-description"
-                    label="Description"
-                    multiline
-                    rows={5}
-                    focused
-                    fullWidth
-                    sx={{ mb: 3 }}
-                // defaultValue="Hello World"
-                />
-                <Box>
-                    <Button variant="contained" sx={{ mr: 3 }}>
-                        Submit
-                    </Button>
-                    <Button variant="contained">
-                        Reset
-                    </Button>
-                </Box>
-            </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <NavbarComponent />
+      <Container maxWidth="xl">
+        <h1>Form Add Item</h1>
+        <TextField
+          required
+          id="item-name"
+          label="Name"
+          focused
+          sx={{ mb: 3 }}
+          fullWidth
+        />
+        <TextField
+          id="outlined-number"
+          label="Quantity"
+          type="number"
+          required
+          fullWidth
+          focused
+          InputLabelProps={{
+            shrink: true,
+          }}
+          sx={{ mb: 3 }}
+        />
+        <TextField
+          id="item-description"
+          label="Description"
+          multiline
+          rows={5}
+          focused
+          fullWidth
+          sx={{ mb: 3 }}
+          // defaultValue="Hello World"
+        />
+        <Box>
+          <Button variant="contained" sx={{ mr: 3 }} endIcon={<SaveIcon />}>
+            <Typography>Submit</Typography>
+          </Button>
+          <Button variant="contained" endIcon={<RestartAltIcon />}>
+            <Typography>Reset</Typography>
+          </Button>
+        </Box>
+      </Container>
+    </>
+  );
+};
 
 export { Add };
